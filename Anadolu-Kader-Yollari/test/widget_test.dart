adca9537d2f01d9ed56800dd40372034815e27cb
fatihistan1199 <fatihistan1199@gmail.com>
2026-09-07 22:04:25 +0300

    Anadolu Kader Yollari smoke testini düzelt

---
+++ Anadolu-Kader-Yollari/test/widget_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:anadolu_kader_yollari/main.dart';
+
+void main() {
+  testWidgets('ana menü açılır', (tester) async {
+    await tester.pumpWidget(const KaderApp());
+    await tester.pumpAndSettle();
+    expect(find.text('ANADOLU'), findsOneWidget);
+    expect(find.text('Yeni Oyun'), findsOneWidget);
+  });
+}
